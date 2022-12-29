06a2821e99418766abffc679798cbe5ee3e29e5f
Danisole <danisole82@gmail.com>
2022-12-29 00:58:22 -0300

    mi primer commit java

---
+++ MainPrueba.java
@@ -0,0 +1,22 @@
+public class MainPrueba{
+    
+    public static void main(String[] args) {
+        //tipos de variables 
+        int numero1 = 24;
+        byte num2 = 1;
+        boolean variable3= true;
+
+        //diferentes usos
+
+        int suma = numero1 + num2;
+        System.out.println(suma);
+
+        if(variable3 == true){
+            System.out.println("esta sentencia es verdadera");
+        }else{
+            System.out.println("es falsa");
+        }
+
+
+    }
+}
